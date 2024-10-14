02933371ac5bcaf24578b906ace25219218dd711
Bilal HAFRI <hafribilal@gmail.com>
2024-10-14 23:50:53 +0100

    setup bouquets list page

--- src/app/dashboard/features/bundle/bouquets-list/bouquets-list.component.ts
+++ src/app/dashboard/features/bundle/bouquets-list/bouquets-list.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { catchError, of, Subject } from 'rxjs';
+import { BouquetList } from 'src/app/shared/models/bouquet';
+import { Page } from 'src/app/shared/models/page';
+import { BouquetService } from 'src/app/shared/services/bouquet.service';
 
 @Component({
   selector: 'app-bouquets-list',
-  standalone: true,
-  imports: [],
   templateUrl: './bouquets-list.component.html',
   styleUrl: './bouquets-list.component.scss'
 })
 export class BouquetsListComponent {
+  displayedColumns: string[] = [
+    'chk',
+    'id',
+    'bouquetName',
+    'streams',
+    'movies',
+    'series',
+    'stations',
+    'action',
+  ];
 
+  dataSource = new MatTableDataSource<BouquetList>([]);
+  totalElements = 0;
+  pageSize = 10;
+  pageIndex = 0;
+  sortDirection = 'asc';
+  sortActive = 'id';
+  loading: boolean = true;
+
+  private searchSubject = new Subject<string>();
+  searchValue = '';
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private bouquetService: BouquetService) {
+    this.loadBouquets();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  filter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  deleteBouquet(id: number): void {
+    if (confirm('Are you sure you want to delete this record?')) {
+      this.bouquetService.deleteBouquet(id).subscribe(() => {
+        this.dataSource.data = this.dataSource.data.filter(bouquet => bouquet.id !== id);
+      });
+    }
+  }
+
+  loadBouquets(): void {
+    const page = this.paginator?.pageIndex || this.pageIndex;
+    const size = this.paginator?.pageSize || this.pageSize;
+
+    this.bouquetService.getBouquets<BouquetList>('', page, size).pipe(
+      catchError(error => {
+        console.error('Failed to load bouquets', error);
+        this.loading = false;
+        return of({ content: [], totalPages: 0, totalElements: 0, size: 0, number:0 } as Page<BouquetList>);
+      })
+    ).subscribe(pageResponse => {
+      this.dataSource.data = pageResponse.content;
+      this.totalElements = pageResponse.totalElements;
+      this.loading = false;
+    });
+  }
 }
--- src/app/dashboard/features/bundle/bundle.module.ts
+++ src/app/dashboard/features/bundle/bundle.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { bundleRoutes } from './bundle.routes';
 import { PackagesListComponent } from './packages-list/packages-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { BouquetsListComponent } from './bouquets-list/bouquets-list.component';
 
 
 
 @NgModule({
-  declarations: [PackagesListComponent],
+  declarations: [PackagesListComponent, BouquetsListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(bundleRoutes),
